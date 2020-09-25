23dac122f30693de17f0b84b12d01751968f5147
jaesharma <44082416+jaesharma@users.noreply.github.com>
2020-09-26 01:44:02 +0530

    minor fixes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
-const path = require("path");
 const express = require("express");
-const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const path = require("path");
 const port = process.env.PORT || 3000;
-
-app.use(express.static(publicPath));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(publicPath, "index.html"));
+const app = express();
+const root = require("path").join(__dirname, "..", "build");
+app.use(express.static(root));
+app.get("*", function (req, res) {
+	res.sendFile(path.join(root, "index.html"));
 });
-
 app.listen(port, () => {
-	console.log("express server is running");
+	console.log("server is up");
 });
