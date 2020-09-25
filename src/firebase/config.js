065989b6d12b8eb48383449c83e3565ded0b81cb
jaesharma <44082416+jaesharma@users.noreply.github.com>
2020-09-26 01:57:53 +0530

    env var set

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import "firebase/storage";
 import "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.FIREBASE_DATABASE_URL,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
-	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_APIKEY,
+	authDomain: process.env.REACT_APP_AUTHDOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASEURL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_API_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 firebase.initializeApp(firebaseConfig);
